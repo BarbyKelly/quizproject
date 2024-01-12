5abfd6e52818b299c610d906e38b3de7d0bfa460
Barby Kelly <barbykelly1@gmail.com>
2024-01-12 15:17:02 +0000

    Add questions.js file, add questions, links, images

---
+++ assets/js/questions.js
@@ -0,0 +1,135 @@
+/* Questions for Estonia Quiz. Layout structure used from https://github.com/andreas-ka/leeds-quiz/blob/main/assets/js/questions.js */
+const quiz = {
+    numb: 1,
+    question: "Where is Estonia located?",
+    answer: "Northern Europe",
+    options: ["Northern Europe",
+              "Asia",
+              "Australia"
+    ]
+}
+
+/* Correct answer to be made hidden, and then pop up for all of the questions,
+with image and link on the side, or on the extra page? Add links to readme, and images, or only here? make answer images
+as part of a comment for now, until correct code, where to place them
+Estonia-Quiz/questions/northerneurope.png
+Screenshot from this website:
+https://gisgeography.com/europe-map/ */
+
+{
+    numb: 2,
+    question: "What colours are on Estonian flag?",
+    answer: "blue, black and white",
+    options: ["white, green and blue",
+              "yellow, black and white"'
+              "blue, black and white"
+    ]
+}
+
+/* Estonia-Quiz/questions/estonianflag.png  
+Estonia-Quiz/questions/flagstory.png
+Link to image of Estonia, with the story about Estonian flag colours, used from this website:
+https://www.visitestonia.com/en//estonian-flag */
+
+{
+    numb: 3,
+    question: "What is the capital of Estonia?",
+    answer: "Tallinn",
+    options: ["Tallinn"
+              "Helsinki"
+              "Riga"
+
+/* Add image of Tallinn, and link to visit Estonia, inside the question/answer brackets or separate?*/
+
+    ]
+}
+
+{
+    numb: 4,
+    question: "What is the official language of Estonia?",
+    answer: "Estonian",
+    options: ["English"
+              "German"
+              "Estonian"
+
+/* add image of English Estonian keyboard incidents */
+    ]
+}
+
+{
+    numb: 5,
+    question: "What is the population of Estonia?",
+    answer: "",
+    options: [
+
+/* add link to answer, make up 3 options */
+
+    ]
+}
+
+{
+    numb: 6,
+    question: "Which of these animals lives in Estonia, in the wild?",
+    answer: "Lynx",
+    options: ["Lion",
+              "Lynx",
+              "Leopard"
+
+/* Add image of a lynx. Credit where got the image.
+https://www.visitestonia.com/en/lynx-and-bear-watching-tour-in-alutaguse-and-lahemaa-national-park?gad_source=5&gclid=EAIaIQobChMI5f2Whpa_gwMVhJhQBh28dgSrEAAYASAAEgIPl_D_BwE */
+
+    ]
+}
+
+{
+    numb: 7,
+    question: "Which of these letters are part of the Estonian alphabet?",
+    answer: "õ,ä,ö,ü",
+    options: ["õ,ä,ö,ü"
+              ""
+              ""
+
+/* might change this question, as there are few options which is official alphabet, without foreign letters */
+
+    ]
+}
+
+{
+    numb: 8,
+    question: "Estonia is part of which States?",
+    answer: "Baltic States",
+    options: ["United States"
+              "Baltic States"
+              "....."
+
+/* Add map of Baltic States, with flags maybe */
+
+    ]
+}
+
+{
+    numb: 9,
+    question: "How often does Estonian Singing and dancing festival take place?",
+    answer: "every 5 years",
+    options: ["every 3 years",
+              "every 5 years",
+              "every 7 years"
+
+/* add explanation, as due to cov it changed for few years, info from this website:
+https://www.visitestonia.com/en/why-estonia/welcome-to-the-estonian-song-and-dance-celebration
+due to covid, after 4 years this time:
+Estonia-Quiz/questions/differentyears.png */
+
+    ]
+}
+
+{
+    numb: 10,
+    question: "",
+    answer: "",
+    options: [
+
+/* maybe national symbols, like dark rye bread, cornflower, etc something a clear answer */
+
+    ]
+};
