51c2bc7adb381d87e99ba3513529846bcef48296
Barby Kelly <barbykelly1@gmail.com>
2024-01-24 14:22:51 +0000

    Add Eventlisteners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+/* From Love Maths example, document listeners copied */
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function"() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            
+            })
+        })
+    }
+};
+
+document.getElementById("answer-box").addEventListener("keydown",
+    function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
+
 /* javascript example basis for now from https://youtu.be/riDzcEQbX6k?feature=shared */
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById ('question-container')
